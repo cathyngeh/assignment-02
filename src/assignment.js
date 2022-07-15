be2cb8f2e8998bb4d103b8bce5500d14018bff15
Cathy Ngeh <109160679+cathyngeh@users.noreply.github.com>
2022-07-15 22:44:37 +0000

    yarn install, yarn test

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,12 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+let sum=arrayOfNumbers.reduce((a,i)=>a+i,0);
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +29,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbers){
+  let count=0;
+  for(let even of arrayOfNumbers){
+    if (even%2==0){
+      count+=1;
+    }
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+  return count
+}
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
